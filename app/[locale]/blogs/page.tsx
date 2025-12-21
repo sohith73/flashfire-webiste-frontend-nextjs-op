0bf4e8cc46b9532049e05db846a4e4d43f678635
support1122 <support@flashfirejobs.com>
2025-12-21 13:00:32 +0530

    Update page.tsx

--- app/[locale]/blogs/page.tsx
+++ app/[locale]/blogs/page.tsx
@@ -39,7 +39,7 @@ export async function generateMetadata({ params }: LocaleBlogsPageProps): Promis
       type: "website",
       images: [
         {
-          url: "https://www.flashfirejobs.com/images/flashfire-logo.png",
+          url: "https://www.flashfirejobs.com/images/og-image.png",
           width: 1200,
           height: 630,
           alt: "FLASHFIRE Logo",
@@ -48,7 +48,7 @@ export async function generateMetadata({ params }: LocaleBlogsPageProps): Promis
     },
     twitter: {
       card: "summary_large_image",
-      images: ["https://www.flashfirejobs.com/images/flashfire-logo.png"],
+      images: ["https://www.flashfirejobs.com/images/og-image.png"],
     },
   };
 }
